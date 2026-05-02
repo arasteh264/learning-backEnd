48f246fdf2f1e350b46f847386c4d4ad150427f6
arasteh264 <arasteh264@gmail.com>
2026-05-02 17:28:05 +0330

    dev-teacherRequest

--- routes/v1/teacher.js
+++ routes/v1/teacher.js
@@ -5,5 +5,7 @@ const isAdminMiddleware=require("../../middlewares/IsAdmin");
 const router=express.Router();
 router.route("/").post(authMiddleware,isAdminMiddleware,teacherController.createTeacher)
 router.route("/").get(authMiddleware,isAdminMiddleware,teacherController.getAllTeacher)
-
+router.route("/request").post(authMiddleware,teacherController.requestForTeacher)
+router.route("/verify/:id").post(authMiddleware,isAdminMiddleware,teacherController.verifyTeacher)
+router.route("/expertises").get( authMiddleware,teacherController.getExpertiseList);
 module.exports=router;
